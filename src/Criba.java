43c5fa5927e8ebb5bef78c5399a3a6b2ba509ef6
miguel angel <mrodriguezrueda24@gmail.com>
2024-03-04 01:13:34 +0100

    Añadimos la documentacion al programa.

--- src/Criba.java
+++ src/Criba.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 
+/**
+ * La clase Criba nos permite generar los numeros primos de un numero determinado.
+ */
 public class Criba {
+    /**
+     * El metodo generarPrimos nos proporciona los numeros primos de la variable int que deseemos.
+     * @param max Variable que indica el numero del cual obtendremos los numeros primos
+     */
     public static int[] generarPrimos(int max) {
         if (max < 2) {
             return new int[0];
@@ -11,6 +18,11 @@ public class Criba {
         }
     }
 
+    /**
+     *Crea un array de booleanos y comprueba si son numeros primos de la variable max.
+     * @param max En este caso max es la variable que define la longitud del array.
+     * @return Devuelve el array con los booleanos definidos.
+     */
     private static boolean[] inicializarPrimos(int max) {
         boolean[] esPrimo = new boolean[max + 1];
         for (int i = 2; i < esPrimo.length; i++) {
@@ -20,6 +32,10 @@ public class Criba {
         return esPrimo;
     }
 
+    /**
+     * Marca como no primos los múltiplos de los números primos encontrados.
+     * @param max Define la longitud de la tabla.
+     */
     private static void cribarMultiplos(int max, boolean[] esPrimo) {
         for (int i = 2; i * i <= max; i++) {
             if (esPrimo[i]) {
@@ -30,6 +46,11 @@ public class Criba {
         }
     }
 
+    /**
+     * Nos proporciona los numeros primos.
+     * @param max Longitud maxima de la tabla.
+     * @return Un array que contiene los numeros que son primos.
+     */
     private static int[] obtenerPrimos(int max, boolean[] esPrimo) {
         int cuenta = contarPrimos(max, esPrimo);
         int[] primos = new int[cuenta];
@@ -41,6 +62,10 @@ public class Criba {
         return primos;
     }
 
+    /**
+     * Cuenta cuantos numeros primos tenemos.
+     * @return La cantidad de números primos encontrados.
+     */
     private static int contarPrimos(int max, boolean[] esPrimo) {
         int cuenta = 0;
         for (boolean primo : esPrimo) {
@@ -51,6 +76,10 @@ public class Criba {
         return cuenta;
     }
 
+    /**
+     * El metodo principal del programa que solicita un numero al usuario y calcula cuantos numeros primos hay
+     * hasta ese numero.
+     */
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Eratóstenes:");
